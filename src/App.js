b713b5deb82205a553f704a2bcee08ea96d6ae16
ziad <helmyzez@gmail.com>
2024-05-23 12:00:56 +0200

    update-nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from './pages/Home/Home';
 import Layout from './components/Layout';
 import Login from './pages/Login/Login';
 import Siginin from './pages/Signin/Siginin';
+import About from './components/about/About';
 
 
 let routers = createBrowserRouter([
   {path:'',element:<Layout/>,children:[ 
     {index:true,element:<Home/>}, 
     {path:'login',element:<Login/>}, 
+    {path:'about',element:<About/>}, 
     {path:'signup',element:<Siginin/>}, 
     // {path:'*',element:<Notfound/>},
   ]}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,64 +3,45 @@ import { Link } from 'react-router-dom'
 import style from './Navbar.module.css'
 const Navbar = () => {
   return <>
-        <div class={`${style.header} bg-dark`} id="header">
-        <div class={style.container}>
-            <Link to={'/'} class={style.logo}>YLY</Link>
-              <ul class={style.mainNav}>
-                  <li><a title='tst' href="#about">About</a></li>
-                  <li><a title='tst' href="#ourGoals">Goals</a></li>
-                  <li><a title='tst' href="#news">News</a></li>
-                  <li><a title='tst' href="#board">Board</a></li>
-                  <li><a title='tst' href="#governments">Governments</a></li>
-              </ul>
-              <ul className={style.social}>
-                <li className='pe-3'><a title='tst' href="#"><i class="fab fa-facebook-f fs-5"></i></a></li>
-                <li className='pe-3'><a title='tst' href="#"><i class="fa-brands fa-instagram fs-5"></i></a></li>
-                <li className='pe-3'><a title='tst' href="#"><i class="fa-brands fa-tiktok fs-5"></i></a></li>
-                <li className='pe-3 '><a title='tst' href="#"><i class="fa-brands fa-linkedin fs-5"></i></a></li>
-                <li className='pe-3 '><Link to={'login'} title='tst' href="#"><span>login</span></Link></li>
-                <li className='pe-3 '><Link to={'signup'} title='tst' href="#"><span>sign up</span></Link></li>
-              </ul>            
-        </div>
-    </div>
-    {/* <nav class="navbar navbar-expand-lg bg-transperent navBack">
-  <div class="container">
-    <a class={style.logo} href="#">YLY</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class={`${style.header} bg-dark navbar navbar-expand-lg z-1`}>
+  <div class="container-fluid">
+    <Link class={`${style.logo} navbar-brand`} to={'/'}>YLY</Link>
+
+    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav m-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Our Goals</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">News</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Board</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Governments</a>
-        </li>
-      </ul>
 
-      <ul className="navbar-nav ms-auto">
-      <li className={`nav-item d-flex fs-4 ${style.sochover} align-items-center`}>
-          <a className={style.sochover} href="" target='_blank'><i className='fab fa-facebook mx-2'></i></a>
-          <a className={style.sochover} href="" target='_blank'><i class="fa-brands fa-tiktok"></i></a>
-          <a className={style.sochover} href="" target='_blank'><i className='fab fa-instagram mx-2'></i></a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class={`${style.newNav}  align-items-center  navbar-nav m-auto mb-2 mb-lg-0 `}>
+        <li class="nav-item">
+          <Link title='tst' class="nav-link " to={'about'}>About</Link>
+        </li>
+        <li class="nav-item">
+          <Link title='tst' class="nav-link " to={'ourGoals'}>Our Goals</Link>
+        </li>
+        <li class="nav-item">
+          <Link title='tst' class="nav-link " to={'news'}>News</Link>
+        </li>
+        <li class="nav-item">
+          <Link title='tst' class="nav-link " to={'board'}>Board</Link>
+        </li>
+        <li class="nav-item">
+          <Link title='tst' class="nav-link " to={'governments'}>Governments</Link>
         </li>
       </ul>
+      
+      <ul className={`${style.social}  align-items-center navbar-nav ms-auto mb-2 mb-lg-0`}>
+                <li className='pe-3'><a class="nav-link " title='tst' href="#"><i class="fab fa-facebook-f fs-5"></i></a></li>
+                <li className='pe-3'><a class="nav-link " title='tst' href="#"><i class="fa-brands fa-instagram fs-5"></i></a></li>
+                <li className='pe-3'><a class="nav-link " title='tst' href="#"><i class="fa-brands fa-tiktok fs-5"></i></a></li>
+                <li className='pe-3 '><a class="nav-link " title='tst' href="#"><i class="fa-brands fa-linkedin fs-5"></i></a></li>
+                <li className='pe-3 '><Link class="nav-link " to={'login'} title='tst' href="#"><span>login</span></Link></li>
+                <li className='pe-3 '><Link class="nav-link " to={'signup'} title='tst' href="#"><span>sign up</span></Link></li>
+      </ul> 
+
     </div>
   </div>
-</nav> */}
+</nav>
   </>
 }
 
